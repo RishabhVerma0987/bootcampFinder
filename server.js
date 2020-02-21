6c5dcec404bc4b55ef1729f680fb5a97cb2f92ac
rishabh verma <vermarishabh0987@gmail.com>
2020-02-21 15:59:01 +0530

    added morgan package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
+const morgan = require("morgan");
 
 //config path
 dotenv.config({ path: "./config/config.env" });
@@ -8,8 +9,9 @@ dotenv.config({ path: "./config/config.env" });
 //Routes import
 const bootcamps = require("./routes/bootcamps.js");
 
-//define main route
+//define main route & middleware
 app.use("/api/v1/bootcamps", bootcamps);
+app.use(morgan("dev"));
 
 //port declaration and port listen
 const port = process.env.PORT || 8000;
