97306aa90304960323bd05c6d6bf4936b4d84531
rishabh verma <vermarishabh0987@gmail.com>
2020-02-21 15:13:18 +0530

    added few bootcamps routes in serve.js

--- server.js
+++ server.js
@@ -6,8 +6,42 @@ dotenv.config({ path: "./config/config.env" });
 
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("<h2>SUCESS</h2>");
+//show all bootcamps
+app.get("/api/V1/bootcamps", (req, res) => {
+  res.status(200).send({
+    sucess: true,
+    body: "show all bootcamps"
+  });
+});
+
+//get a single bootcamp
+app.get("/api/V1/bootcamps/:id", (req, res) => {
+  res.status(200).send({
+    sucess: true,
+    body: `show bootcamp ${req.params.id}`
+  });
+});
+
+//create new bootcamp
+app.post("/api/V1/bootcamps", (req, res) => {
+  res.status(200).send({
+    sucess: true,
+    body: "create new bootcamp"
+  });
+});
+//update a bootcamps
+app.put("/api/V1/bootcamps/:id", (req, res) => {
+  res.status(200).send({
+    sucess: true,
+    body: `update bootcamp ${req.params.id}`
+  });
+});
+//delete a bootcamps
+app.delete("/api/V1/bootcamps/:id", (req, res) => {
+  res.status(200).send({
+    sucess: true,
+    body: `delete bootcamp ${req.params.id}`
+  });
 });
 
 app.listen(
