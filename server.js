79bd2a8f543039d62a8b1a23c20048885b800885
rishabh verma <vermarishabh0987@gmail.com>
2020-02-22 14:34:06 +0530

    added mongoose and done few initial setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const morgan = require("morgan");
+const conncetDB = require("./config/db.js");
 
 //config path
 dotenv.config({ path: "./config/config.env" });
@@ -10,12 +11,18 @@ dotenv.config({ path: "./config/config.env" });
 const bootcamps = require("./routes/bootcamps.js");
 
 //define main route & middleware
-app.use("/api/v1/bootcamps", bootcamps);
 app.use(morgan("dev"));
+app.use("/api/v1/bootcamps", bootcamps);
+conncetDB();
 
 //port declaration and port listen
 const port = process.env.PORT || 8000;
-app.listen(
+const server = app.listen(
   port,
   console.log(`Server running in ${process.env.NODE_ENV} mode on PORT:${port}`)
 );
+
+process.on("unhandledRejection", (err, promisee) => {
+  console.log(`Error : ${err.message}`);
+  server.close(() => process.exit(1));
+});
