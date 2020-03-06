6078c68619c8785d4385ce810d64d996dd6498c0
rishabh verma <vermarishabh0987@gmail.com>
2020-03-06 14:18:39 +0530

    Added errorhandler for duplicate fields and validation

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,10 +1,20 @@
 const ErrorHandler = require("../utils/errorHandler.js");
 
 const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+  let error = { ...err };
+  console.log(err);
 
   if (err.name == "CastError") {
-    error = new ErrorHandler(`Bootcamp not found at id ${err.value}`);
+    error = new ErrorHandler(`Bootcamp not found at id ${err.value}`, 400);
+  }
+
+  if (err.code == 11000) {
+    error = new ErrorHandler(`Duplicate Fields`, 400);
+  }
+
+  if (err.name == "ValidationError") {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorHandler(`${message}`, 400);
   }
 
   res.status(error.statusCode || 500).json({
