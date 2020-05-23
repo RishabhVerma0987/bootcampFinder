a95e816cb745efe01506d16b5ab05115ae107ea1
rishabh verma <vermarishabh0987@gmail.com>
2020-05-23 13:45:09 +0530

    course schema and seeder

---
+++ models/Course.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add a title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+  },
+  weeks: {
+    type: String,
+    required: [true, "Please specify weeks"],
+  },
+  tuition: {
+    type: Number,
+    required: [true, "please enter tuition"],
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, "please enter mininum skill"],
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Bootcamp",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Course", CourseSchema);
--- seeder.js
+++ seeder.js
@@ -7,24 +7,29 @@ dotenv.config({ path: "./config/config.env" });
 
 //load model schema
 const BootcampModel = require("./models/Bootcamp.js");
+const CourseModel = require("./models/Course.js");
 
 //connectToDB
 mongoose.connect(process.env.DATABASE_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 
 //readJsonFile
 const bootcamp = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+const course = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
 
 //Populate database
 const ImportData = async () => {
   try {
     await BootcampModel.create(bootcamp);
-    console.log("Data Imported".green.inverse);
+    await CourseModel.create(course);
+    console.log(`Data Imported `.green.inverse);
     process.exit();
   } catch (err) {
     console.log(err);
@@ -35,6 +40,7 @@ const ImportData = async () => {
 const DeleteData = async () => {
   try {
     await BootcampModel.deleteMany();
+    await CourseModel.deleteMany();
     console.log("Data Deleted".red.inverse);
     process.exit();
   } catch (err) {
@@ -44,8 +50,8 @@ const DeleteData = async () => {
 
 //args setup CLI
 
-if (process.argv[2] == "-import") {
+if (process.argv[2] === ("-i" || "-import")) {
   ImportData();
-} else if (process.argv[2] == "-delete") {
+} else if (process.argv[2] === ("-d" || "-delete")) {
   DeleteData();
 }
